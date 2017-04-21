89c691ab0eb77b6fad8b0760d9a67b42791c24f6
zeroCalm <smith.timothyh@gmail.com>
2017-04-20 18:57:42 -0700

    Articles created verified associations are working

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,4 +2,6 @@ class Location < ApplicationRecord
   geocoded_by :location_address, :latitude  => :lat, :longitude => :lng
   after_validation :geocode, :if => lambda{ |obj| obj.location_address? }
 
+  has_many :articles, dependent: :destroy
+  has_many :users, through: :articles
 end
