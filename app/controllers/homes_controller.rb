efddadc08c2081c49115cc0ad32691c8d3e7b244
zeroCalm <smith.timothyh@gmail.com>
2017-04-30 20:32:39 -0700

    background and lyout major overhaul

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,16 +1,9 @@
 class HomesController < ApplicationController
   before_action :authenticate_user!
 
-
   def index
     if current_user
-        redirect_to locations_path
+      redirect_to map_path
     end
-  end
-
-  
-
-  def mapAdd
-    @locations = Location.all
   end
 end
