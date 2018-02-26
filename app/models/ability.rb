783cc9555828077ee7dea0316fb5210ff0d8d7c7
zeroCalm <smith.timothyh@gmail.com>
2018-02-26 07:50:48 -0800

    work on heroku error

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,8 +2,8 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    # can :access, :rails_admin   # grant access to rails_admin
-    # can :dashboard              # grant access to the dashboard
+     can :access, :rails_admin   # grant access to rails_admin
+     can :dashboard              # grant access to the dashboard
     # can :read, :all                   # allow everyone to read everything
     # Define abilities for the passed in user here. For example:
     #
